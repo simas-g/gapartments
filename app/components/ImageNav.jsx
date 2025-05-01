71591dcda7f3f5dd5f6ee3f04de04dc2a8c46b1e
Simas G <simas@simson.lt>
2025-05-01 14:45:46 +0300

    fixed nav + img optimization and next-intl

--- app/components/ImageNav.jsx
+++ app/components/ImageNav.jsx
@@ -1,67 +1,64 @@
-'use client'
-import { useState, useRef, useEffect } from "react"
-import { X, ChevronLeft, ChevronRight } from 'lucide-react';
+"use client";
+import { useState, useRef, useEffect } from "react";
+import { X, ChevronLeft, ChevronRight } from "lucide-react";
+import Image from "next/image";
 const ImageNav = ({ images }) => {
-  const [viewing, setViewing] = useState(false)
-  const [selected, setSelected] = useState(0)
-  const scrollBar = useRef()
+  const [viewing, setViewing] = useState(false);
+  const [selected, setSelected] = useState(0);
+  const scrollBar = useRef();
   //main image
-  const mainImage = images[0]
+  const mainImage = images[0];
   //mid images
-  const midImages = images.slice(1, 3)
+  const midImages = images.slice(1, 3);
   //smaller images
-  const imagePreviews = images.slice(3, 8)
+  const imagePreviews = images.slice(3, 8);
 
   const handleImageChange = (dir) => {
-    if (dir === 'right') {
+    if (dir === "right") {
       if (selected < images?.length - 1) {
-        setSelected((prev) => prev + 1)
-      } else setSelected(0)
+        setSelected((prev) => prev + 1);
+      } else setSelected(0);
     } else {
       if (selected > 0) {
-        setSelected((prev) => prev - 1)
-      } else setSelected(images?.length - 1)
+        setSelected((prev) => prev - 1);
+      } else setSelected(images?.length - 1);
     }
-  }
+  };
   const handleImage = (i) => {
-    setSelected(i)
-  }
+    setSelected(i);
+  };
   const handleImagesView = () => {
     setViewing(true);
-  }
+  };
 
   const thumbsRef = useRef([]);
 
   const scrollToThumb = (index) => {
-    if(thumbsRef.current[index]) {
+    if (thumbsRef.current[index]) {
       thumbsRef.current[index].scrollIntoView({
         behavior: "instant",
         inline: "center",
         block: "nearest",
       });
     }
-
   };
   useEffect(() => {
     if (viewing) {
       document.body.style.overflow = "hidden";
     } else {
-      document.body.style.overflow = ""; 
+      document.body.style.overflow = "";
     }
-  
+
     return () => {
       document.body.style.overflow = "";
     };
   }, [viewing]);
-  
+
   // Scroll when selected changes
-    useEffect(() => {
-      scrollToThumb(selected);
-      setTimeout(() => {
-
-      }, 500);
-    }, [selected]);
-
+  useEffect(() => {
+    scrollToThumb(selected);
+    setTimeout(() => {}, 500);
+  }, [selected]);
 
   return (
     <div className="max-h-90 grid grid-cols-3 grid-rows-4 gap-2 max-w-4xl w-full h-auto rounded-lg overflow-hidden">
@@ -69,79 +66,94 @@ const ImageNav = ({ images }) => {
       {viewing && (
         <div className="fixed inset-0 z-100 bg-white px-6 py-6">
           <div className="w-full flex justify-end">
-            <div className="cursor-pointer flex items-center gap-x-1 hover:underline" onClick={() => setViewing(false)}>
-              <p >Uždaryti</p>
-              <X width={40} height={40} color="black"/>
+            <div
+              className="cursor-pointer flex items-center gap-x-1 hover:underline"
+              onClick={() => setViewing(false)}
+            >
+              <p>Uždaryti</p>
+              <X width={40} height={40} color="black" />
             </div>
           </div>
 
           <div className="flex flex-col justify-center items-center gap-y-4 mt-8">
             <div className="select-none border border-gray-300 w-80 h-80 max-[90%] sm:w-100 sm:h-[450px] overflow-hidden flex items-center">
-              <img src={images[selected]} className="object-cover object-center w-full h-auto" alt="" />
+              <img
+                src={images[selected]}
+                className="object-cover object-center w-full h-auto"
+                alt=""
+              />
             </div>
             <div className="text-black">
               {selected + 1} / {images.length}
             </div>
             <div className="flex gap-x-6">
-              <div onClick={()=> handleImageChange('left')} className="sm:absolute top-[30%] sm:top-[40%] left-5 cursor-pointer">
-                <ChevronLeft width={50} height={50} color="black"/>
+              <div
+                onClick={() => handleImageChange("left")}
+                className="sm:absolute top-[30%] sm:top-[40%] left-5 cursor-pointer"
+              >
+                <ChevronLeft width={50} height={50} color="black" />
               </div>
-              <div onClick={()=> handleImageChange('right')} className="sm:absolute top-[30%] sm:top-[40%] right-5 cursor-pointer">
-                <ChevronRight width={50} height={50} color="black"/>
+              <div
+                onClick={() => handleImageChange("right")}
+                className="sm:absolute top-[30%] sm:top-[40%] right-5 cursor-pointer"
+              >
+                <ChevronRight width={50} height={50} color="black" />
               </div>
             </div>
 
-            <div ref={scrollBar} 
-            className="flex gap-x-4 overflow-x-auto max-w-[390px] md:max-w-[680px] px-4 scroll-smooth"
-            style={{ scrollbarWidth: "none" }}
-
+            <div
+              ref={scrollBar}
+              className="flex gap-x-4 overflow-x-auto max-w-[390px] md:max-w-[680px] px-4 scroll-smooth"
+              style={{ scrollbarWidth: "none" }}
             >
-              
-            {images.map((image, i) => (
+              {images.map((image, i) => (
                 <div
-
                   ref={(el) => (thumbsRef.current[i] = el)}
                   key={i}
                   className={`select-none w-20 h-20 flex-shrink-0 overflow-hidden cursor-pointer rounded-sm ${
-                    i === selected ? 'border-2 border-black' : 'opacity-40'
+                    i === selected ? "border-2 border-black" : "opacity-40"
                   }`}
                   onClick={() => handleImage(i)}
                 >
-                  <img src={image} className="object-cover rounded-sm w-full h-full" alt="" />
+                  <img
+                    src={image}
+                    className="object-cover rounded-sm w-full h-full"
+                    alt=""
+                  />
                 </div>
               ))}
             </div>
-
           </div>
-          </div>
-          
+        </div>
       )}
-      {/* main image */}
-      <div className="col-span-2 h-auto w-full flex items-center overflow-hidden row-span-3 cursor-pointer select-none" style={{
-        borderRadius: '8px 0px 0px 0px'
-      }} onClick={() => handleImagesView()}>
-
-        <img
-          src={mainImage}
-          className="object-cover w-full h-full"
-          alt=""
-        />
+      <div
+        className="col-span-2 w-full relative flex items-center overflow-hidden row-span-3 cursor-pointer select-none"
+        style={{
+          borderRadius: "8px 0px 0px 0px",
+        }}
+        onClick={() => handleImagesView()}
+      >
+        <Image priority src={mainImage} fill alt="gapartments" className="object-cover" />
       </div>
 
       {/* mid images */}
-      <div className="flex flex-col gap-2 col-span-1 row-span-3 select-none"
-      style={{
-        borderRadius: '0px 8px 0px 0px'
-      }} >
+      <div
+        className="flex flex-col gap-2 col-span-1 row-span-3 select-none"
+        style={{
+          borderRadius: "0px 8px 0px 0px",
+        }}
+      >
         {midImages.map((image, i) => (
           <div
             key={i}
-            className="overflow-hidden cursor-pointer flex h-full w-full items-center"
+            className="overflow-hidden cursor-pointer flex h-full w-full items-center relative"
             onClick={() => handleImagesView()}
           >
-            <img
+            <Image
               src={image}
-              className={`object-cover w-full md:h-auto h-full object-center`}
+              fill
+              priority
+              className={`object-cover`}
               alt=""
             />
           </div>
@@ -149,33 +161,34 @@ const ImageNav = ({ images }) => {
       </div>
 
       {/* Image Previews */}
-    <div className="grid grid-cols-5 gap-2 col-span-3 row-span-1 overflow-hidden select-none place-items-center">
-      {imagePreviews.map((image, i) => (
-        <div
-          key={i}
-          className="overflow-hidden h-full w-full cursor-pointer rounded-sm relative"
-          onClick={() => handleImagesView()}
-          
-        >
-          {/* Overlay for the last image preview */}
-          {i === imagePreviews.length - 1 && (
-            <div style={{
-              backdropFilter: 'blur(6px)',
-            }} className="absolute inset-0 flex items-center justify-center bg-black/50 text-white font-bold rounded-sm sm:text-xs text-[10px] px-2">
-              Peržiūrėti visas nuotraukas ({images.length})
-            </div>
-          )}
-          <img
-            src={image}
-            className={`object-cover rounded-sm w-full md:h-auto h-full`}
-            alt=""
-          />
-        </div>
-      ))}
+      <div className="grid grid-cols-5 gap-2 col-span-3 row-span-1 overflow-hidden select-none place-items-center">
+        {imagePreviews.map((image, i) => (
+          <div
+            key={i}
+            className="overflow-hidden h-full w-full cursor-pointer rounded-sm relative"
+            onClick={() => handleImagesView()}
+          >
+            {/* Overlay for the last image preview */}
+            {i === imagePreviews.length - 1 && (
+              <div
+                style={{
+                  backdropFilter: "blur(6px)",
+                }}
+                className="absolute inset-0 flex items-center justify-center bg-black/50 text-white font-bold rounded-sm sm:text-xs text-[10px] px-2"
+              >
+                Peržiūrėti visas nuotraukas ({images.length})
+              </div>
+            )}
+            <img
+              src={image}
+              className={`object-cover rounded-sm w-full md:h-auto h-full`}
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
     </div>
+  );
+};
 
-    </div>
-  )
-}
-
-export default ImageNav
+export default ImageNav;
--- app/components/TopBar.jsx
+++ app/components/TopBar.jsx
@@ -3,14 +3,14 @@ import { Phone, Mail } from "lucide-react";
 const TopBar = () => {
   return (
     <div className="sticky top-0 w-full h-12 z-8 bg-black">
-      <div className="flex items-center justify-around h-full max-w-7xl mx-auto px-4">
-        <div className="text-white text-sm font-semibold flex gap-x-2">
-            <Mail/>
-            <p>g.gedeikiene@gmail.com</p>
+      <div className="flex items-center justify-around h-full max-w-7xl mx-auto px-4 gap-x-2">
+        <div className="text-white text-xs sm:text-sm font-semibold flex gap-1 items-center md:gap-x-2">
+          <Mail size={'2em'}/>
+          <p>g.gedeikiene@gmail.com</p>
         </div>
-        <div className="text-white flex gap-x-2 text-sm font-semibold">
-            <Phone/>
-            <p>+370 610 95591</p>
+        <div className="text-white flex gap-1 items-center md:gap-x-2 text-xs sm:text-sm font-semibold">
+          <Phone size={'2em'}/>
+          <p>+370 610 95591</p>
         </div>
       </div>
     </div>
